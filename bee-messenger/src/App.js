f08abe376a21b59bfa33ea5257cd38e8775cecd5
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-11-16 13:29:55 +0000

    Add toaster notification button

--- bee-messenger/src/App.js
+++ bee-messenger/src/App.js
@@ -1,5 +1,23 @@
+import toast, { Toaster } from "react-hot-toast";
+
 const App = () => {
-  return <div className="App"></div>;
+  const notify = () => toast("Hello World!",
+  {
+    duration: 3000,
+    icon: '👏',
+    ariaProps: {
+      role: 'status',
+      'aria-live': 'polite'
+    }
+  }
+  );
+
+  return (
+    <div className="App">
+      <button onClick={notify}>Make me a toast</button>
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
